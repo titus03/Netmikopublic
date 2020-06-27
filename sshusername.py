5fd8cefe254411a2f7e7884e7301d3b2a1f74c9c
titus03 <67462521+titus03@users.noreply.github.com>
2020-06-27 17:21:34 +0530

    Basic SSH

---
+++ sshusername.py
@@ -0,0 +1,18 @@
+from netmiko import ConnectHandler
+from datetime import datetime
+from getpass import getpass
+host = input('Enter the Ip address:')
+username  = input('Username:')
+start_time = datetime.now()
+print(start_time)
+cat4331 = {
+    "device_type": "cisco_ios",
+    "ip" : host,
+    "username": username,
+    "password": getpass(),
+}
+netconnect = ConnectHandler(**cat4331)
+output = netconnect.send_command("show arp")
+print(output)
+output = netconnect.send_command("sh int des")
+print(output)
