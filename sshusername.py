0ef068da0b10f9d427f06820334ffedbfc0fc113
titus03 <67462521+titus03@users.noreply.github.com>
2020-06-27 17:57:21 +0530

    Add files via upload

--- sshusername.py
+++ sshusername.py
@@ -3,6 +3,11 @@ from datetime import datetime
 from getpass import getpass
 host = input('Enter the Ip address:')
 username  = input('Username:')
+command1 = input('Enter command 1:')
+command2 = input('Enter command 2:')
+command3 = input('Enter command 3:')
+command4 = input('Enter command 4:')
+command5 = input('Enter command 5:')
 start_time = datetime.now()
 print(start_time)
 cat4331 = {
@@ -12,7 +17,14 @@ cat4331 = {
     "password": getpass(),
 }
 netconnect = ConnectHandler(**cat4331)
-output = netconnect.send_command("show arp")
+output = netconnect.send_command(command1)
 print(output)
-output = netconnect.send_command("sh int des")
+output = netconnect.send_command(command1)
 print(output)
+output = netconnect.send_command(command3)
+print(output)
+output = netconnect.send_command(command4)
+print(output)
+output = netconnect.send_command(command5)
+print(output)
+
